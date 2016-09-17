4ad817f81dca3fa133a56a03fe9348099b0c0e10
sgalutira <sheena.galutira@gmail.com>
2016-09-17 12:08:54 -1000

    step2_html5

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var path = require('path');
 
 var app = express();
 
-app.use(express.static(__dirname + 'public'));
+app.use(express.static(__dirname + '/public'));
+
 
 var server = app.listen(8080, function(){
   var host = server.address().address;
