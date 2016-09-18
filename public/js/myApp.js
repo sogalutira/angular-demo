74648746b1ac82cb5cb0ba7a631e90afce3ad3dd
sgalutira <sheena.galutira@gmail.com>
2016-09-17 14:38:56 -1000

    step10_constant

--- public/js/myApp.js
+++ public/js/myApp.js
@@ -1,1 +1,4 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', [])
+  .run(['$rootScope', 'APP_VERSION', function($rootScope, version){
+    $rootScope.APP_VERSION = version;
+  }]);
